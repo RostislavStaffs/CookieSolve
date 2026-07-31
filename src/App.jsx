f2deaa341e4e562c28db0db9c488153b055577cf
RostislavStaffs <v015989p@student.staffs.ac.uk>
2026-07-31 12:48:40 +0100

    Added more routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import AuthLayout from "./components/AuthLayout";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import NewScanPage from "./pages/NewScanPage";
+import ScanResultsPage from "./pages/ScanResultsPage";
+import ScanHistoryPage from "./pages/ScanHistoryPage";
+import RulesAllowlistPage from "./pages/RulesAllowlistPage";
+import ReportsPage from "./pages/ReportsPage";
+import SettingsPage from "./pages/SettingsPage";
+import HelpAboutPage from "./pages/HelpAboutPage";
 import "./App.css";
 
 function App() {
@@ -11,11 +19,62 @@ function App() {
       <Route path="/" element={<LandingPage />} />
 
       <Route element={<AuthLayout />}>
-        <Route path="/sign-up" element={<RegisterPage />} />
-        <Route path="/sign-in" element={<LoginPage />} />
+        <Route
+          path="/sign-up"
+          element={<RegisterPage />}
+        />
+
+        <Route
+          path="/sign-in"
+          element={<LoginPage />}
+        />
       </Route>
 
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route
+        path="/dashboard"
+        element={<DashboardPage />}
+      />
+
+      <Route
+        path="/new-scan"
+        element={<NewScanPage />}
+      />
+
+      <Route
+        path="/scan-results"
+        element={<ScanResultsPage />}
+      />
+
+      <Route
+        path="/scan-history"
+        element={<ScanHistoryPage />}
+      />
+
+      <Route
+        path="/rules"
+        element={<RulesAllowlistPage />}
+      />
+
+      <Route
+        path="/reports"
+        element={<ReportsPage />}
+      />
+
+      <Route
+        path="/settings"
+        element={<SettingsPage />}
+      />
+
+      <Route
+        path="/help"
+        element={<HelpAboutPage />}
+      />
+
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
+      
     </Routes>
   );
 }
