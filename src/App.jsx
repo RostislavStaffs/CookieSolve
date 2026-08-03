df6262ac70fff3f4c77994ac41093444031c0d3c
RostislavStaffs <v015989p@student.staffs.ac.uk>
2026-08-03 11:41:14 +0100

    User auth and scan functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/AuthLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -19,62 +20,22 @@ function App() {
       <Route path="/" element={<LandingPage />} />
 
       <Route element={<AuthLayout />}>
-        <Route
-          path="/sign-up"
-          element={<RegisterPage />}
-        />
-
-        <Route
-          path="/sign-in"
-          element={<LoginPage />}
-        />
+        <Route path="/sign-up" element={<RegisterPage />} />
+        <Route path="/sign-in" element={<LoginPage />} />
       </Route>
 
-      <Route
-        path="/dashboard"
-        element={<DashboardPage />}
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/new-scan" element={<NewScanPage />} />
+        <Route path="/scan-results" element={<ScanResultsPage />} />
+        <Route path="/scan-history" element={<ScanHistoryPage />} />
+        <Route path="/rules" element={<RulesAllowlistPage />} />
+        <Route path="/reports" element={<ReportsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/help" element={<HelpAboutPage />} />
+      </Route>
 
-      <Route
-        path="/new-scan"
-        element={<NewScanPage />}
-      />
-
-      <Route
-        path="/scan-results"
-        element={<ScanResultsPage />}
-      />
-
-      <Route
-        path="/scan-history"
-        element={<ScanHistoryPage />}
-      />
-
-      <Route
-        path="/rules"
-        element={<RulesAllowlistPage />}
-      />
-
-      <Route
-        path="/reports"
-        element={<ReportsPage />}
-      />
-
-      <Route
-        path="/settings"
-        element={<SettingsPage />}
-      />
-
-      <Route
-        path="/help"
-        element={<HelpAboutPage />}
-      />
-
-      <Route
-        path="*"
-        element={<Navigate to="/" replace />}
-      />
-      
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
