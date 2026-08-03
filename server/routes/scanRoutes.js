327ad42abe4518dac40a439185af6e7d5675ddd1
RostislavStaffs <v015989p@student.staffs.ac.uk>
2026-08-03 13:12:24 +0100

    Added delete button and re-try scan func.

--- server/routes/scanRoutes.js
+++ server/routes/scanRoutes.js
@@ -2,18 +2,38 @@ import express from "express";
 
 import {
   createScan,
+  deleteScan,
   getScan,
   getScans,
 } from "../controllers/scanController.js";
 
 import requireAuth from "../middleware/requireAuth.js";
 
-const scanRouter = express.Router();
+const scanRouter =
+  express.Router();
 
-scanRouter.use(requireAuth);
+scanRouter.use(
+  requireAuth,
+);
 
-scanRouter.post("/", createScan);
-scanRouter.get("/", getScans);
-scanRouter.get("/:scanId", getScan);
+scanRouter.post(
+  "/",
+  createScan,
+);
+
+scanRouter.get(
+  "/",
+  getScans,
+);
+
+scanRouter.get(
+  "/:scanId",
+  getScan,
+);
+
+scanRouter.delete(
+  "/:scanId",
+  deleteScan,
+);
 
 export default scanRouter;
